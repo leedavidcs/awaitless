e4b90e3aa91fbdfa25bb7f19aea1e643f6e337e6
David Lee <dlee@onfleet.com>
2019-08-28 13:09:16 -0700

    Added code-coverage testing with sonarcloud.

---
+++ scripts/generate-sonar/index.ts
@@ -0,0 +1,43 @@
+import { writeFile } from "fs-extra";
+import { Environment, FileSystemLoader } from "nunjucks";
+import ReadPkg, { NormalizedPackageJson } from "read-pkg";
+
+const testInclusionPatterns: string[] = ["**/*test.ts*", "**/*test/index.ts*"];
+const exclusionPatterns: string[] = [
+	".jest/**/*",
+	"coverage/**/*",
+	"dist/**/*",
+	"node_modules/**/*",
+	"scripts/**/*",
+	"test-reports/**/*",
+	"**/*test.ts*",
+	"**/*test/index.ts*",
+	"**/*.js*"
+];
+
+/**
+ * @function main
+ * @returns { never }
+ * @description Generates a sonar-project.properties file for SonarCloud static analysis
+ */
+const main = async (): Promise<never> => {
+	const pkg: NormalizedPackageJson = await ReadPkg();
+	const environment: Environment = new Environment(
+		new FileSystemLoader("scripts/generate-sonar")
+	);
+
+	const inclusions: string = testInclusionPatterns.join(",");
+	const exclusions: string = exclusionPatterns.join(",");
+
+	const fileContents: string = environment.render("sonar-project.njk", {
+		exclusions,
+		inclusions,
+		pkg
+	});
+
+	await writeFile("sonar-project.properties", fileContents);
+
+	return process.exit(0);
+};
+
+main();
